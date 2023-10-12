099d0f7556c6d86bc16ee73ba75cc4cdc5fc4bce
Stephen Hanna <stephenhanna@c02gf0b9ml85.client.kainos.com>
2023-10-12 13:50:51 +0100

    working on it

--- 005-create-project-delivery-employee-table.sql
+++ 005-create-project-delivery-employee-table.sql
@@ -1,12 +1,15 @@
 USE ThePrimaryKeys_DylanC;
 
 CREATE table project_delivery_employee (
-	project_delivery_emplopyee_id mediumint unsigned NOT NULL,
+	project_delivery_emplopyee_id mediumint unsigned NOT NULL AUTO_INCREMENT,
 	project_id smallint unsigned NOT NULL,
 	delivery_employee_id smallint unsigned NOT NULL,
 	started_on_date date NOT NULL,
 	ended_on_date date,
 	PRIMARY KEY (project_delivery_emplopyee_id),
 	FOREIGN KEY (project_id) REFERENCES project(project_id),
-	FOREIGN KEY (delivery_employee_id) REFERENCES delivery_employee(delivery_employee_id)
+	FOREIGN KEY (delivery_employee_id) REFERENCES delivery_employee(employee_id)
 );
+
+INSERT INTO project_delivery_employee (project_id, delivery_employee_id, started_on_date)
+VALUES ()
