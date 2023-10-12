b5cb6ffef1ef92f7992425cea4e6bea5bb0ed331
40175096 <mmcnicholl10@qub.ac.uk>
2023-10-12 13:56:43 +0100

    megan 002 create delivery employee second try

--- 002CreateDeliveryEmployee.sql
+++ 002CreateDeliveryEmployee.sql
@@ -3,18 +3,40 @@
 
 USE ThePrimaryKeys_DylanC;
 
+-- creating delivery_employee table
 CREATE TABLE delivery_employee (
-	delivery_employee_id smallint unsigned NOT NULL, 
-    employee_id smallint unsigned NOT NULL
+    employee_id smallint unsigned AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY (employee_id)
+	REFERENCES employee(employee_id)
 );
 
-ALTER TABLE delivery_employee
-	ADD PRIMARY KEY(delivery_employee_id);
-    
-ALTER TABLE delivery_employee
-ADD CONSTRAINT fk_delivery_employee_employee_id
-FOREIGN KEY (employee_id)
-REFERENCES employee(employee_id);
-
 DESCRIBE delivery_employee;
-    
+
+-- add employees to employee table then add them to delivery_employee table to make them delivery employees
+INSERT INTO employee 
+(first_name, last_name, salary, bank_account_number, NI_number)
+VALUES ('doctor', 'who', 35000, 10295674, 'PG836602Y');
+
+INSERT INTO employee 
+(first_name, last_name, salary, bank_account_number, NI_number)
+VALUES ('bob', 'mcgee', 40000, 10299067, 'TR838602Y');
+
+INSERT INTO employee 
+(first_name, last_name, salary, bank_account_number, NI_number)
+VALUES ('alice', 'mcfadden', 53000, 05673829, 'HG729602K');
+
+-- insert new employees into delivery_employee table
+INSERT INTO delivery_employee
+(employee_id) 
+VALUES (3);
+
+INSERT INTO delivery_employee
+(employee_id) 
+VALUES (4);
+
+INSERT INTO delivery_employee
+(employee_id) 
+VALUES (5);
+
+SELECT * FROM delivery_employee;
+
