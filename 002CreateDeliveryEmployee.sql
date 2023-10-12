5c3e48ae60c7e5b02da1be6edbac96977370a628
40175096 <mmcnicholl10@qub.ac.uk>
2023-10-12 14:34:36 +0100

    third try 002 create delivery employee

--- 002CreateDeliveryEmployee.sql
+++ 002CreateDeliveryEmployee.sql
@@ -10,21 +10,6 @@ CREATE TABLE delivery_employee (
 	REFERENCES employee(employee_id)
 );
 
-DESCRIBE delivery_employee;
-
--- add employees to employee table then add them to delivery_employee table to make them delivery employees
-INSERT INTO employee 
-(first_name, last_name, salary, bank_account_number, NI_number)
-VALUES ('doctor', 'who', 35000, 10295674, 'PG836602Y');
-
-INSERT INTO employee 
-(first_name, last_name, salary, bank_account_number, NI_number)
-VALUES ('bob', 'mcgee', 40000, 10299067, 'TR838602Y');
-
-INSERT INTO employee 
-(first_name, last_name, salary, bank_account_number, NI_number)
-VALUES ('alice', 'mcfadden', 53000, 05673829, 'HG729602K');
-
 -- insert new employees into delivery_employee table
 INSERT INTO delivery_employee
 (employee_id) 
@@ -38,5 +23,10 @@ INSERT INTO delivery_employee
 (employee_id) 
 VALUES (5);
 
-SELECT * FROM delivery_employee;
+-- create right join with employee table to see id of delivery employee and name
+SELECT delivery_employee.employee_id, CONCAT(first_name, ' ', last_name) AS `Name` 
+FROM delivery_employee
+RIGHT JOIN employee
+ON delivery_employee.employee_id = employee.employee_id
+WHERE delivery_employee.employee_id =5;
 
