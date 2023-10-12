7d02f6a7125efb9eedd2fa6da5bf7e9d2cf11194
Stephen Hanna <stephenhanna@c02gf0b9ml85.client.kainos.com>
2023-10-12 14:14:00 +0100

    added sample data

--- 005-create-project-delivery-employee-table.sql
+++ 005-create-project-delivery-employee-table.sql
@@ -1,5 +1,7 @@
 USE ThePrimaryKeys_DylanC;
 
+-- Create table
+
 CREATE table project_delivery_employee (
 	project_delivery_emplopyee_id mediumint unsigned NOT NULL AUTO_INCREMENT,
 	project_id smallint unsigned NOT NULL,
@@ -11,5 +13,23 @@ CREATE table project_delivery_employee (
 	FOREIGN KEY (delivery_employee_id) REFERENCES delivery_employee(employee_id)
 );
 
+
+-- Add sample data
+
 INSERT INTO project_delivery_employee (project_id, delivery_employee_id, started_on_date)
-VALUES ()
+VALUES (9, 3, '2023-11-12');
+INSERT INTO project_delivery_employee (project_id, delivery_employee_id, started_on_date)
+VALUES (11, 3, '2023-11-12');
+INSERT INTO project_delivery_employee (project_id, delivery_employee_id, started_on_date)
+VALUES (9, 4, '2023-11-12');
+INSERT INTO project_delivery_employee (project_id, delivery_employee_id, started_on_date)
+VALUES (11, 5, '2023-11-12');
+INSERT INTO project_delivery_employee (project_id, delivery_employee_id, started_on_date, ended_on_date)
+VALUES (12, 3, '2023-10-14', '2023-11-12');
+
+
+-- Check data is there
+
+SELECT project_delivery_emplopyee_id, project_id, delivery_employee_id, started_on_date, ended_on_date  
+FROM project_delivery_employee pde ;
+
