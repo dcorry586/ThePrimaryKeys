93a884b0615134c41e14e64852de75ddd05874e6
dcorry586 <dylancorr15@gmail.com>
2023-10-12 13:35:00 +0100

    create client table added auto increment and select and insert

--- 003-create-client-table.sql
+++ 003-create-client-table.sql
@@ -2,14 +2,14 @@
 USE ThePrimaryKeys_DylanC;
 
 -- Create client table
-CREATE TABLE client(
-	client_id SMALLINT UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS client(
+	client_id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
 	name VARCHAR(255) NOT NULL,
 	address VARCHAR(255) NOT NULL,
 	phone_number VARCHAR(11) NOT NULL,
-	sales_employee_id SMALLINT UNSIGNED NOT NULL,
 	PRIMARY KEY (client_id),
-	FOREIGN KEY (sales_employee_id) REFERENCES sales_employee(sales_employee_id)
+    sales_employee_id SMALLINT UNSIGNED NOT NULL,
+    FOREIGN KEY (sales_employee_id) REFERENCES sales_employee(employee_id)
 );
 
 -- Insert data into table
@@ -17,5 +17,4 @@ INSERT INTO client (name, address, phone_number, sales_employee_id)
 VALUES ('Bob Dylan', '123 somewhere over the rainbow St', '07935754333', 1);
 
 -- Select a record from table
-SELECT * FROM client;
-
+SELECT * FROM sales_employee;
